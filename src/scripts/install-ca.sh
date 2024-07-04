3f1c044cd914ccf60948810d3259d394bf28986c
ppoi <ppoi@tsukuba-bunko.org>
2024-07-04 12:25:56 +0000

    correct type-charlie tag name

--- src/scripts/install-ca.sh
+++ src/scripts/install-ca.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 
 CA_DATA=""
 if echo "$2" | grep -E '^http(s)?://.*$' > /dev/null; then
@@ -10,9 +11,9 @@ if echo "$2" | grep -E '^http(s)?://.*$' > /dev/null; then
     echo "install curl"
     sudo /opt/install-apt-packages.sh curl
   fi
-  CERT_DATA=$(curl -kL ${CERT_URL})
+  CERT_DATA=$(curl -kL "$2")
 elif [ -f "$2" ]; then
-  echo "cert from file"
+  echo "cert from file: $2"
   CERT_DATA=$(cat "$2")
 fi
 
@@ -27,5 +28,5 @@ sudo update-ca-certificates
 
 if which keytool > /dev/null; then
   echo "install CA cert into JVM default keystore."
-  keytool -cacerts -import -alias ${CANAME} -storepass "${JVM_KEYSTORE_PASS:-"changeit"}" -f "/usr/local/share/ca-certificates/${CANAME}.crt"
+  sudo keytool -cacerts -import -alias ${CANAME} -storepass "${JVM_KEYSTORE_PASS:-"changeit"}" -f "/usr/local/share/ca-certificates/${CANAME}.crt"
 fi
