54339be87d60a5420ba26edd32ea7488deeb66bb
ppoi <ppoi@tsukuba-bunko.org>
2026-05-14 19:30:39 +0900

    [update] pnpm 11.1

--- src/files/entrypoint.mod/pnpm-store.sh
+++ src/files/entrypoint.mod/pnpm-store.sh
@@ -4,7 +4,7 @@ if [ ! -d /workspaces/.pnpm-store ]; then
   sudoIf mkdir /workspaces/.pnpm-store
   sudoIf chown $(id -u):$(id -g) /workspaces/.pnpm-store
 fi
-PNPM=$(which pnpm || ls ~/.local/share/pnpm/pnpm 2>/dev/null || true)
+PNPM=$(which pnpm || ls ~/.local/share/pnpm/pnpm 2>/dev/null || ls ~/.local/share/pnpm/bin/pnpm 2>/dev/null || true)
 if [ -z "${PNPM}" ]; then
   echo "pnpm may not be installed. skip configure store-dir"
 else
